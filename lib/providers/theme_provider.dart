2d78ced4b91b0bfcdd7822afd213238975f6b0f1
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-07 23:52:32 +0530

    added theme provider

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Themeprovider with ChangeNotifier {
+  ThemeData _currentTheme = ThemeData.light();
+
+  ThemeData get currentTheme => _currentTheme;
+
+  void toggleTheme() {
+    _currentTheme = _currentTheme == ThemeData.light()
+        ? ThemeData.dark()
+        : ThemeData.light();
+
+    notifyListeners();
+  }
+}
