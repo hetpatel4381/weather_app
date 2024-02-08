1bde348df4b6dee2f40558347054b9cb22092391
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-08 23:13:46 +0530

    updated home_view.dart

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import '../../constants/constants.dart' as k;
+// import 'constants.dart' as k;
+import 'dart:convert';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
-
   @override
-  State<HomeView> createState() => _HomeViewState();
+  _HomeViewState createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
@@ -12,11 +16,13 @@ class _HomeViewState extends State<HomeView> {
   late int temp;
   late int press;
   late int hum;
-  String cityName = "";
+  late int cover;
+  String cityname = '';
   TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     getCurrentLocation();
   }
@@ -24,50 +30,347 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xfffa8bff),
-              Color(0xff2bd2ff),
-              Color(0xff2bff88),
-            ],
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xffFA8BFF),
+                Color(0xff2BD2FF),
+                Color(0xff2BFF88),
+              ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+            ),
           ),
-        ),
-        child: Visibility(
-          visible: isLoaded,
-          child: Column(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.85,
-                height: MediaQuery.of(context).size.height * 0.09,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.3),
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                ),
-                child: Center(
-                  child: TextFormField(
-                    onFieldSubmitted: (String s) {
-                      setState(() {
-                        cityName = s;
-                        getCityWeather(s);
-                      });
-                    },
+          child: Visibility(
+            visible: isLoaded,
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: MediaQuery.of(context).size.height * 0.09,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.3),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        20,
+                      ),
+                    ),
+                  ),
+                  child: Center(
+                    child: TextFormField(
+                      onFieldSubmitted: (String s) {
+                        setState(() {
+                          cityname = s;
+                          getCityWeather(s);
+                          isLoaded = false;
+                          controller.clear();
+                        });
+                      },
+                      controller: controller,
+                      cursorColor: Colors.white,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Search city',
+                        hintStyle: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white.withOpacity(0.7),
+                          fontWeight: FontWeight.w600,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.search_rounded,
+                          size: 25,
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Icon(
+                        Icons.pin_drop,
+                        color: Colors.red,
+                        size: 40,
+                      ),
+                      Text(
+                        cityname,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade900,
+                        offset: const Offset(1, 2),
+                        blurRadius: 3,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Image(
+                        image: const AssetImage('images/thermometer.png'),
+                        width: MediaQuery.of(context).size.width * 0.09,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Temperature: ${temp?.toInt()} ºC',
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade900,
+                        offset: const Offset(1, 2),
+                        blurRadius: 3,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image(
+                          image: const AssetImage('images/barometer.png'),
+                          width: MediaQuery.of(context).size.width * 0.09,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Pressure: ${press?.toInt()} hPa',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade900,
+                        offset: const Offset(1, 2),
+                        blurRadius: 3,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image(
+                          image: const AssetImage('images/humidity.png'),
+                          width: MediaQuery.of(context).size.width * 0.09,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Humidity: ${hum?.toInt()}%',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade900,
+                        offset: const Offset(1, 2),
+                        blurRadius: 3,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image(
+                          image: const AssetImage('images/cloud cover.png'),
+                          width: MediaQuery.of(context).size.width * 0.09,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Cloud Cover: ${cover?.toInt()}%',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            replacement: const Center(
+              child: CircularProgressIndicator(),
+            ),
           ),
         ),
       ),
-    ));
+    );
+  }
+
+  getCurrentLocation() async {
+    var p = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+      forceAndroidLocationManager: true,
+    );
+    if (p != null) {
+//      print('Lat:${p?.latitude}, Long:${p?.longitude}');
+      getCurrentCityWeather(p);
+    } else {
+      print('Data unavailable');
+    }
+  }
+
+  getCityWeather(String cityname) async {
+    var client = http.Client();
+    var uri = '${k.domain}q=$cityname&appid=${k.apiKey}';
+    var url = Uri.parse(uri);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodeData = json.decode(data);
+//      print(data);
+      updateUI(decodeData);
+      setState(() {
+        isLoaded = true;
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  getCurrentCityWeather(Position position) async {
+    var client = http.Client();
+    var uri =
+        '${k.domain}lat=${position.latitude}&lon=${position.longitude}&appid=${k.apiKey}';
+    var url = Uri.parse(uri);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodeData = json.decode(data);
+      print(data);
+      updateUI(decodeData);
+      setState(() {
+        isLoaded = true;
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  updateUI(var decodedData) {
+    setState(() {
+      if (decodedData == null) {
+        temp = 0;
+        press = 0;
+        hum = 0;
+        cover = 0;
+        cityname = 'Not available';
+      } else {
+        temp = decodedData['main']['temp'] - 273;
+        press = decodedData['main']['pressure'];
+        hum = decodedData['main']['humidity'];
+        cover = decodedData['clouds']['all'];
+        cityname = decodedData['name'];
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
   }
 }
